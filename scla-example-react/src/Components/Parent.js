a93e82411d66e9e810d27361fb2fa1c8fb8bc598
Miguel Menjivar <83032119+TxT-Miguel@users.noreply.github.com>
2022-03-30 09:46:36 -0700

    sample files for slides

--- scla-example-react/src/Components/Parent.js
+++ scla-example-react/src/Components/Parent.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import Child from "./Child";
 
 const Parent = () => {
+    const [count, setCount] = useState(0);
+
+    const countHandler = () => {
+        setCount(count + 1);
+    }
+
+
     return (
         <div>
             <Child passingData="Hello from parent"/>
+
+            <h1>{count}</h1>
+
+            <button onClick={countHandler}>Increment</button>
         </div>
     )
 }
